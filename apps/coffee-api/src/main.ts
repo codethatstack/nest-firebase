5e091e4241996fb7155caae742ef478df8403d04
Anthony Miller <anthonymiller74@outlook.com>
2020-10-25 19:34:58 +1000

    Update main.ts for firebase functions export

--- apps/coffee-api/src/main.ts
+++ apps/coffee-api/src/main.ts
@@ -1,21 +1,23 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
+const server = express();
 
-bootstrap();
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+    .then(v => console.log('Nest Ready'))
+    .catch(err => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
